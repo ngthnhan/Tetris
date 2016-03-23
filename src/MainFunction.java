6d80a31f221efd9d7c4004371799cc170e318e0a
CodingArtifice <ngthnhan93@gmail.com>
2016-03-23 16:57:57 +0800

    Added class and placeholder to put code in

---
+++ src/MainFunction.java
@@ -0,0 +1,48 @@
+/**
+ * Created by ThanhNhan on 23/03/2016.
+ */
+public class MainFunction {
+    // Write your functions here
+
+    // Feature 1
+
+    // Feature 2
+
+    // Feature 3
+
+    // Feature 4
+
+    // Feature 5
+
+    // Feature 6
+
+    // Feature 7
+
+    // Feature 8
+
+    // Feature 9
+
+    // Feature 10
+
+
+    // Implementation of f1
+
+    // Implementation of f2
+
+    // Implementation of f3
+
+    // Implementation of f4
+
+    // Implementation of f5
+
+    // Implementation of f6
+
+    // Implementation of f7
+
+    // Implementation of f8
+
+    // Implementation of f9
+
+    // Implementation of f10
+
+}
