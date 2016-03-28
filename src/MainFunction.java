00c29bb3e26a861e6e6c33fb87c300fb41e7f5a5
ngthnhan <ngthnhan93@gmail.com>
2016-03-28 10:07:03 +0800

    Implement feature 1 2 3

--- src/MainFunction.java
+++ src/MainFunction.java
@@ -1,14 +1,17 @@
 /**
  * Created by ThanhNhan on 23/03/2016.
  */
+
 public class MainFunction {
     // Write your functions here
 
     // Feature 1
 
+
     // Feature 2
 
     // Feature 3
+
 
     // Feature 4
 
@@ -26,11 +29,71 @@ public class MainFunction {
 
 
     // Implementation of f1
+    double getLandingHeight(State s, int action) {
+        int orient = s.legalMoves()[action][State.ORIENT];
+        int slot = s.legalMoves()[action][State.SLOT];
+        return getLandingHeight(s, orient, slot);
+    }
+    /**
+     * The height at where the piece is put = height of column + (height of piece / 2)
+     * @param s To get the board position and how it will change according to the action
+     * @param orient To orientation of the piece
+     * @param slot The slot index of the left most column
+     * @return The heuristic score for the given state and action taken.
+     */
+    double getLandingHeight(State s, int orient, int slot) {
+        int piece = s.getNextPiece();
 
+        double maxHeight = -1;
+        for (int i = 0, col = slot; i < s.getpWidth()[piece][orient]; i++, col++) {
+            double height = s.getTop()[col] - s.getpBottom()[piece][orient][i];
+            maxHeight = Math.max(maxHeight,  height + s.getpHeight()[piece][orient] / 2.0);
+        }
+        return maxHeight;
+    }
     // Implementation of f2
+    int getErodedPieces(State s, int action) {
+        int orient = s.legalMoves()[action][State.ORIENT];
+        int slot = s.legalMoves()[action][State.SLOT];
+        return getErodedPieces(s, orient, slot);
+    }
 
+    int getErodedPieces(State s, int orient, int slot) {
+        State nextState = new State(s);
+        nextState.makeMove(orient, slot);
+        return s.getRowsCleared() - nextState.getRowsCleared();
+    }
     // Implementation of f3
+    int getRowTransition(State s) {
+        int transCount = 0;
+        for (int i = 0; i < State.ROWS - 1; i++) {
+            if(s.getField()[i][0] == 0) transCount++;
+            if(s.getField()[i][State.COLS-1] == 0) transCount++;
+            for(int j=1;j<State.COLS;j++) {
+                if (s.getField()[i][j] != s.getField()[i][j - 1]) {
+                    transCount++;
+                }
+            }
+        }
+        return transCount;
+    }
 
+    int getRowTransition(State s, int action) {
+        State nextState = new State(s);
+        nextState.makeMove(action);
+
+        int transCount = 0;
+        for (int i = 0; i < State.ROWS - 1; i++) {
+            if(nextState.getField()[i][0] == 0) transCount++;
+            if(nextState.getField()[i][State.COLS-1] == 0) transCount++;
+            for(int j=1;j<State.COLS;j++) {
+                if (nextState.getField()[i][j] != nextState.getField()[i][j - 1]) {
+                    transCount++;
+                }
+            }
+        }
+        return transCount;
+    }
     // Implementation of f4
 
     // Implementation of f5
